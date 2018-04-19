0e97f879dce70181e462029853aafe8539b04225
DouglasDiaz <douglasmolinskidiaz@gmail.com>
2018-04-19 17:45:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ disco.on('message', message => {
   }
   
   if (message.content === "d>blablabla") {
-    message.reply("/tts blablabla?");
+    message.reply("blablabla?");
   } else if (message.content === "d>startdisco" && partyOn == false) {
     //if (allowedUsers.includes(message.author.id)) {
       message.reply("Sure thing boss!");
