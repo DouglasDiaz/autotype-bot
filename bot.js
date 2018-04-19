8a5eec4697506ae1bc5ab51175f0812dc757ea3a
DouglasDiaz <douglasmolinskidiaz@gmail.com>
2018-04-19 13:20:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,20 @@ client.on('message', message => {
   if (message.content === "blablabla") {
     message.reply("blablabla?");
   }
+  if (message.substring(0, 1) == '>') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            case 'blablabla':
+                client.sendMessage({
+                    to: channelID,
+                    message: 'Blablabla'
+                });
+            break;
+         }
+     }
 });
 
 client.login(process.env.BOT_TOKEN);
