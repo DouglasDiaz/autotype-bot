fe67f488bbee25d91f881950170c38829295d6d2
DouglasDiaz <douglasmolinskidiaz@gmail.com>
2018-04-19 13:44:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,48 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({
-  autorun: true,
-  token: process.env.BOT_TOKEN
-});
+"use strict";
 
-client.on('ready', () => {
+const Discord = require('discord.js');
+const disco = new Discord.Client();
+const prefix = "d>;
+const allowedUsers = config.allowedUsers;
+const roles = ["🅺🅸🅽🅶", "𝓠𝓾𝓮𝓮𝓷"];
+
+disco.on('ready', () => {
   console.log('Connected');
 });
 
-client.on('message', message => {
-  /*if (message.content === "blablabla") {
+disco.on('message', message => {
+  if (message.content === ">blablabla") {
     message.reply("blablabla?");
-  }*/
-  if (message.substring(0, 1) == '>') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            case 'blablabla':
-                client.sendMessage({
-                    to: channelID,
-                    message: 'Blablabla'
-                });
-            break;
-         }
-     }
+  }
+  
+  function discoRole() {
+    roles.forEach((role) => {
+      let theRole = message.guild.roles.find("name", role);
+      let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+      theRole.edit({color: random}).catch(e => {
+        return message.channel.sendMessage(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
+      });
+    });
+  }
+
+  if(message.content.startsWith(prefix + "startdisco")) {
+    if(allowedUsers.includes(message.author.id)) {
+    setInterval(() => { discoRole(); }, config.ms);
+    message.channel.sendMessage("```css\nDiscoing...```");
+    message.channel.sendMessage("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
+  } else {
+    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+} else
+
+if(message.content.startsWith(prefix + "stopdisco")) {
+  if(allowedUsers.includes(message.author.id)) {
+  message.channel.sendMessage("I've stopped discoing.");
+  setTimeout(() => { console.log(process.exit(0)); }, 300);
+} else {
+  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+}
 });
 
-//client.login(process.env.BOT_TOKEN);
+disco.login(process.env.BOT_TOKEN);
